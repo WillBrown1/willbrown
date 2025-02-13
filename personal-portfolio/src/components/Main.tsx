a75d22c33773c691d54f4bbbaef3fe23a8ee9478
will <willparker514@gmail.com>
2025-02-13 12:30:22 -0500

    now we're cooking

--- personal-portfolio/src/components/Main.tsx
+++ personal-portfolio/src/components/Main.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Typewriter from 'typewriter-effect';
 import '../assets/styles/Main.scss';
+import avatar from '../assets/images/avatar.png';
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={avatar} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/Willbrown1" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/will-brown-66872b251/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+
+          <h1>Will Brown</h1>
+          <p>
+            <Typewriter
+              options={{
+                strings: ["Full Stack Engineer", "Software Developer", "Tech Enthusiast", "Problem Solver"],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+              }}
+            />
+          </p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
