69b3623579b662b88f597299a18e7f27bcddcf3e
will <willparker514@gmail.com>
2025-02-12 21:24:56 -0500

    a

--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -1,6 +1,6 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import logo1 from "../assets/img/Csharplogo.png";
+import logo2 from "../assets/img/reactlogo.png";
+import logo3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -38,20 +38,20 @@ export const Skills = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={logo1} alt="Image" />
+                                <h5>C#</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <img src={logo2} alt="Image" />
+                                <h5>Vite</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <img src={logo3} alt="Image" />
+                                <h5>React</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={logo1} alt="Image" />
+                                <h5>something else</h5>
                             </div>
                         </Carousel>
                     </div>
